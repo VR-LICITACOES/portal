fee842ea364c9aede9090bcea228939d0649eef6
Miguel Alves <malvscode@gmail.com>
2026-03-18 23:35:35 -0300

    Update server.js

--- apps/portal/server.js
+++ apps/portal/server.js
@@ -119,7 +119,7 @@ router.post('/api/logout', async (req, res) => {
   }
 });
 
-// Rota curinga: serve o index.html para qualquer rota não-API (SPA)
+// Rota curinga: serve o index.html para qualquer rota não-API
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
