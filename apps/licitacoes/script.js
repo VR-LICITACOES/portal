de55a19f561577b71c1c36c250e4510c0fc83324
Miguel Alves <malvscode@gmail.com>
2026-04-02 12:31:13 -0300

    Update script.js

--- apps/licitacoes/script.js
+++ apps/licitacoes/script.js
@@ -291,22 +291,8 @@ function renderLicitacoes(lista) {
             </td>
             <td class="acoes-col" onclick="event.stopPropagation()">
                 <div class="acoes-btns">
-                    <button class="btn-acao btn-acao-editar" onclick="openFormModal('${l.id}')" title="Editar">
-                        <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2">
-                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-                        </svg>
-                        Editar
-                    </button>
-                    <button class="btn-acao btn-acao-excluir" onclick="openDeleteModal('${l.id}')" title="Excluir">
-                        <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2">
-                            <polyline points="3 6 5 6 21 6"></polyline>
-                            <path d="M19 6l-1 14H6L5 6"></path>
-                            <path d="M10 11v6M14 11v6"></path>
-                            <path d="M9 6V4h6v2"></path>
-                        </svg>
-                        Excluir
-                    </button>
+                    <button class="action-btn edit" onclick="openFormModal('${l.id}')" title="Editar">Editar</button>
+                    <button class="action-btn delete" onclick="openDeleteModal('${l.id}')" title="Excluir">Excluir</button>
                 </div>
             </td>
         </tr>`;
